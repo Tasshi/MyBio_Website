c39a1c636a56e0f4f78f622388b8c8129aa22fd5
Tashi <tasheephuntsho301@gmail.com>
2026-04-27 18:28:12 +0600

    Fix case sensitivity and update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/about";
+import About from "./pages/About";
 import Navbar from "./components/Navbar";
 import "./App.css";
 
